4eb6882778c93de2cf8a885f7a004cf8fba428c3
neelgupta <neelgupta701@gmail.com>
2024-12-13 19:44:15 +0530

    add validation in contact form

--- src/pages/User/ViewInteraction/AnswerForm/ContactForm.jsx
+++ src/pages/User/ViewInteraction/AnswerForm/ContactForm.jsx
@@ -6,7 +6,8 @@ import "./AnswerForm.scss";
 import { handelCatch, throwError } from "../../../../store/globalSlice";
 import { api } from "../../../../services/api";
 import { Spinner } from "react-bootstrap";
-
+import { useFormik } from "formik";
+import * as Yup from "yup";
 function ContactForm({ onNext, node, isPost }) {
   console.log("node", node);
   const dispatch = useDispatch();
@@ -14,6 +15,7 @@ function ContactForm({ onNext, node, isPost }) {
   const [form, setForm] = useState({
     email: "",
   });
+  const [validationSchema, setValidationSchema] = useState({});
 
   useEffect(() => {
     fetchContactForm();
@@ -32,6 +34,25 @@ function ContactForm({ onNext, node, isPost }) {
     if (contactFormData.is_product) {
       setFormData["product"] = "";
     }
+
+    setValidationSchema({
+      name: contactFormData?.is_name
+        ? Yup.string().required("Name is required")
+        : Yup.string(),
+      email: contactFormData?.is_email
+        ? Yup.string()
+            .email("Invalid email format")
+            .required("Email is required")
+        : Yup.string(),
+      phone: contactFormData?.is_phone
+        ? Yup.number()
+            .typeError("Invalid phone number")
+            .required("Phone number is required")
+        : Yup.number(),
+      product: contactFormData?.is_product
+        ? Yup.string().required("Product name is required")
+        : Yup.string(),
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [contactFormData]);
 
@@ -52,84 +73,115 @@ function ContactForm({ onNext, node, isPost }) {
     }
   };
 
-  // const
+  const validationForm = () => {
+    let valid = true;
+    valid = Object.keys(form).forEach((key) => {
+      if (!form[key]) {
+        return false;
+      }
+    });
+    return valid;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      phone: "",
+      product: "",
+    },
+    validationSchema: Yup.object(validationSchema),
+    onSubmit: (values) => {
+      if (!isPost) {
+        onNext(values);
+      }
+    },
+  });
   return (
     <div className="ContactForm-container">
       <div className="text-20-600 mb-30 " style={{ color: "#8000ff" }}>
         Before you go, please leave your contact details so we can get back to
         you...
       </div>
-      <div className="ContactForm">
+      <form onSubmit={formik.handleSubmit} className="ContactForm">
         <div className="form">
           {contactFormData?.is_name && (
-            <div className="mb-20">
-              <div className={`text-12-600 mb-5`} style={{ color: "#666666" }}>
-                name :
-              </div>
-              <div className="wp-100 InputBox">
-                <input
-                  type="text"
-                  name="name"
-                  value={form.name}
-                  onChange={(e) => setForm({ ...form, name: e.target.value })}
-                  placeholder="Enter name"
-                  className="form-control"
-                />
-              </div>
+            <div className="mb-20 InputBox">
+              <label className="text-12-600 mb-5" style={{ color: "#666666" }}>
+                Name:
+              </label>
+              <input
+                type="text"
+                name="name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Enter name"
+                className="form-control"
+              />
+              {formik.touched.name && formik.errors.name && (
+                <div className="error-text">{formik.errors.name}</div>
+              )}
             </div>
           )}
 
           {contactFormData?.is_email && (
-            <div className="mb-20">
-              <div className={`text-12-600 mb-5`} style={{ color: "#666666" }}>
-                email :
-              </div>
-              <div className="wp-100 InputBox">
-                <input
-                  type="email"
-                  value={form.email}
-                  onChange={(e) => setForm({ ...form, email: e.target.value })}
-                  name="email"
-                  placeholder="Enter email"
-                  className="form-control"
-                />
-              </div>
+            <div className="mb-20 InputBox">
+              <label className="text-12-600 mb-5" style={{ color: "#666666" }}>
+                Email:
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Enter email"
+                className="form-control"
+              />
+              {formik.touched.email && formik.errors.email && (
+                <div className="error-text">{formik.errors.email}</div>
+              )}
             </div>
           )}
+
           {contactFormData?.is_phone && (
-            <div className="mb-20">
-              <div className={`text-12-600 mb-5`} style={{ color: "#666666" }}>
-                Phone number :
-              </div>
-              <div className="wp-100 InputBox">
-                <input
-                  type="number"
-                  value={form.phone}
-                  onChange={(e) => setForm({ ...form, phone: e.target.value })}
-                  name="phone_number"
-                  placeholder="Enter phone number"
-                  className="form-control"
-                />
-              </div>
+            <div className="mb-20 InputBox">
+              <label className="text-12-600 mb-5" style={{ color: "#666666" }}>
+                Phone number:
+              </label>
+              <input
+                type="number"
+                name="phone"
+                value={formik.values.phone}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Enter phone number"
+                className="form-control"
+              />
+              {formik.touched.phone && formik.errors.phone && (
+                <div className="error-text">{formik.errors.phone}</div>
+              )}
             </div>
           )}
+
           {contactFormData?.is_product && (
-            <div className="mb-20">
-              <div className={`text-12-600 mb-5`} style={{ color: "#666666" }}>
-                Product name :
-              </div>
-              <div className="wp-100 InputBox">
-                <input
-                  type="text"
-                  value={form.product}
-                  onChange={(e) =>
-                    setForm({ ...form, product: e.target.value })
-                  }
-                  name="product_name"
-                  placeholder="Enter product name"
-                  className="form-control"
-                />
-              </div>
+            <div className="mb-20 InputBox">
+              <label className="text-12-600 mb-5" style={{ color: "#666666" }}>
+                Product name:
+              </label>
+              <input
+                type="text"
+                name="product"
+                value={formik.values.product}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Enter product name"
+                className="form-control"
+              />
+              {formik.touched.product && formik.errors.product && (
+                <div className="error-text">{formik.errors.product}</div>
+              )}
             </div>
           )}
 
@@ -139,15 +191,9 @@ function ContactForm({ onNext, node, isPost }) {
             </ul>
           )}
         </div>
+
         <div id="ans-btn-group">
-          <button
-            className="next-btn"
-            onClick={() => {
-              if (!isPost) {
-                onNext();
-              }
-            }}
-          >
+          <button type="submit" className="next-btn">
             {isPost ? (
               <Spinner size="lg" color="#888888" />
             ) : (
@@ -162,7 +208,7 @@ function ContactForm({ onNext, node, isPost }) {
               />
             )}
           </button>
-          <button className="cancel-btn" onClick={() => {}}>
+          <button type="button" className="cancel-btn" onClick={() => {}}>
             <img
               src={icons.closeSvg}
               alt=""
@@ -171,7 +217,7 @@ function ContactForm({ onNext, node, isPost }) {
             />
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
